75e5112313d8ecfb63b386afc406e8fd275c4ebf
Andrea <and29ferraro@gmail.com>
2023-02-09 16:23:25 +0100

    integration tweet

--- hackatweet/components/Home.js
+++ hackatweet/components/Home.js
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+import Tweet from "./Tweet";
 
 function Home() {
   return (
@@ -25,6 +26,9 @@ function Home() {
             <p>0/280</p>
             <button className={styles.tweetBtn}>Tweet</button>
           </div>
+        </div>
+        <div className={styles.middleBottom}>
+          <Tweet />
         </div>
       </div>
 
